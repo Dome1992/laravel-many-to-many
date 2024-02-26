d471768b424673183c57b7e691882b17ca37ec35
Dome1992 <forlanodomenico92@mail.com>
2024-02-26 17:26:55 +0100

    problemi in down

--- database/migrations/9999_02_24_153436_add_foreign_keys.php
+++ database/migrations/9999_02_24_153436_add_foreign_keys.php
@@ -18,10 +18,9 @@ return new class extends Migration
             $table -> foreignId('type_id') -> constrained();
         });
 
-        Schema::create('project_technology', function (Blueprint $table) {
+        Schema::table('project_technology', function (Blueprint $table) {
 
             $table -> foreignId('project_id') -> constrained();
-
             $table -> foreignId('technology_id') -> constrained();
         });
     }
@@ -38,6 +37,15 @@ return new class extends Migration
             $table -> dropForeign('projects_type_id_foreign');
 
             $table-> dropColumn('type_id');
+        });
+
+        Schema::table('project_technology', function (Blueprint $table) {
+
+            $table -> dropForeign('project_technology_project_id_foreign');
+            $table-> dropColumn('project_id');
+
+            $table -> dropForeign('project_technology_technology_id_foreign');
+            $table-> dropColumn('technology_id');
         });
     }
 };
